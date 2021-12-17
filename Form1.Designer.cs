d21625e2d6617c64e8e913d5122bec6e40197c4e
raulale@student.ubc.ca <raulale@student.ubc.ca>
2021-12-17 14:28:11 -0800

    Added Probability Changer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,12 @@ namespace Hitomezashi_Generator
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +59,7 @@ namespace Hitomezashi_Generator
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 160);
+            this.comboBox1.Location = new System.Drawing.Point(12, 371);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
@@ -61,7 +67,7 @@ namespace Hitomezashi_Generator
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 144);
+            this.label1.Location = new System.Drawing.Point(9, 355);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 13);
             this.label1.TabIndex = 2;
@@ -70,7 +76,7 @@ namespace Hitomezashi_Generator
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Location = new System.Drawing.Point(12, 293);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 13);
             this.label2.TabIndex = 3;
@@ -78,14 +84,14 @@ namespace Hitomezashi_Generator
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 99);
+            this.textBox1.Location = new System.Drawing.Point(15, 310);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(175, 99);
+            this.textBox2.Location = new System.Drawing.Point(175, 310);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
@@ -93,7 +99,7 @@ namespace Hitomezashi_Generator
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(139, 102);
+            this.label3.Location = new System.Drawing.Point(139, 313);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(12, 13);
             this.label3.TabIndex = 6;
@@ -103,11 +109,72 @@ namespace Hitomezashi_Generator
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "To Be Implemented Later";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0.5";
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "0.5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "On Probability";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Off Probability";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 594);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -133,6 +200,12 @@ namespace Hitomezashi_Generator
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,14 +19,16 @@ namespace Hitomezashi_Generator
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            
             var rand = new Random();
-
+            double on_Prob = Convert.ToDouble(textBox3.Text);
+            double off_Prob = Convert.ToDouble(textBox4.Text);
             Graphics g = e.Graphics;
 
             for (int j = 0; j < 500; j = j + 10)
             {
-                int x = rand.Next(100);
-                if (x < 50)
+                double x = rand.Next(100);
+                if (x < on_Prob*100)
                 {
                     for (int i = 0; i < 500; i = i + 20)
                     {
@@ -44,7 +46,7 @@ namespace Hitomezashi_Generator
             for (int i = 0; i < 500; i = i + 10)
             {
                 int x = rand.Next(100);
-                if (x < 50)
+                if (x < on_Prob*100)
                 {
                     for (int j = 0; j < 500; j = j + 20)
                     {
@@ -59,6 +61,17 @@ namespace Hitomezashi_Generator
                     }
                 }
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            double y = 1 - Convert.ToDouble(textBox3.Text);
+            textBox4.Text = Convert.ToString(y);
         }
     }
 }
